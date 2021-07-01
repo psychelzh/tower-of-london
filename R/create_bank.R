e58942c5dd9b5c623ef270b433fdb1dc95fb5257
Liang Zhang <psychelzh@outlook.com>
2021-07-01 17:46:25 +0800

    🐞 fix: remove level rule
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/create_bank.R
+++ R/create_bank.R
@@ -13,13 +13,11 @@ create_bank <- function(graph) {
       pattern_id1 = (from %/% 10 - to %/% 10) %% 6,
       pattern_id2 = paste0(from %% 10, to %% 10),
       pattern_id = paste(pattern_id1, pattern_id2, sep = "_"),
-      time = level * 10,
-      stay = (level * 100 + time * 10) * 0.8,
-      promotion = (level * 100 + time * 10) * 0.9
+      time = level * 10
     ) |>
     group_by(level) |>
     mutate(type = dense_rank(pattern_id)) |>
     ungroup() |>
-    select(from, to, level, type, time, stay, promotion) |>
+    select(from, to, level, type, time) |>
     arrange(level, type)
 }
