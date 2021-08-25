042d7c36e5bb3abe5a5a0e84566e3116df26f37c
Liang Zhang <psychelzh@outlook.com>
2021-08-25 17:00:33 +0800

    remove time
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/create_bank.R
+++ R/create_bank.R
@@ -13,8 +13,7 @@ create_bank <- function(graph) {
       type1_raw = (from %/% 10 - to %/% 10) %% 6,
       type1 = if_else(type1_raw > 3, 6 - type1_raw, type1_raw),
       type2 = paste0(from %% 10, to %% 10),
-      type = paste(type1, type2, sep = "_"),
-      time = level * 10
+      type = paste(type1, type2, sep = "_")
     ) |>
     group_by(level) |>
     mutate(type_id = dense_rank(type)) |>
