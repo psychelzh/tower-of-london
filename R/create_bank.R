dff4ede6c39dc83f99e02f13a473eb8984566950
Liang Zhang <psychelzh@outlook.com>
2021-08-25 17:05:55 +0800

    use `min_move` instead of `level`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/create_bank.R
+++ R/create_bank.R
@@ -6,7 +6,7 @@ create_bank <- function(graph) {
   graph |>
     distances() |>
     as_tibble(rownames = "from") |>
-    pivot_longer(-from, names_to = "to", values_to = "level") |>
+    pivot_longer(-from, names_to = "to", values_to = "min_move") |>
     mutate(across(c(from, to), parse_number)) |>
     filter(from != to) |>
     mutate(
@@ -15,8 +15,8 @@ create_bank <- function(graph) {
       type2 = paste0(from %% 10, to %% 10),
       type = paste(type1, type2, sep = "_")
     ) |>
-    group_by(level) |>
+    group_by(min_move) |>
     mutate(type_id = dense_rank(type)) |>
     ungroup() |>
-    arrange(level, type_id, type)
+    arrange(min_move, type_id, type)
 }
