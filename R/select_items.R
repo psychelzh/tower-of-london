c17e6f7abfa6c16742c71d26410b19e4ea1894b4
Liang Zhang <psychelzh@outlook.com>
2021-08-26 14:46:50 +0800

    add iso-problems
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/select_items.R
+++ R/select_items.R
@@ -1,8 +1,8 @@
 #' Select Items
 #'
-#' The selected all starts from configuration identifier ending with 1. Tasks
-#' are selected from those with minimal moves from 3 to 7, and 4 tasks for each
-#' type of move.
+#' The selected all starts from configuration identifier ending with 5 (or say,
+#' flat type). Tasks are selected from those with minimal moves from 3 to 7, and
+#' 4 tasks for each type of move.
 #'
 #' @title
 #' @param item_bank
@@ -12,11 +12,21 @@
 select_items <- function(item_bank) {
   set.seed(1)
   item_bank |>
-    filter(min_move %in% 3:7, from %% 10 == 1, type1 != 0) |>
-    group_nest(min_move, type_id) |>
+    filter(min_move %in% 3:7, from %% 10 == 5) |>
+    group_nest(min_move, type) |>
     group_by(min_move) |>
-    slice_sample(n = 4) |>
-    ungroup() |>
-    mutate(data = map(data, slice_sample, n = 1)) |>
-    unnest(data)
+    group_modify(
+      ~ .x |>
+        slice_sample(n = 4) |>
+        transmute(
+          tasks = map_chr(
+            data,
+            ~ .x |>
+              mutate(task = str_glue("{from}-{to}")) |>
+              pluck("task") |>
+              str_c(collapse = ";")
+          )
+        )
+    ) |>
+    ungroup()
 }
